3983ed1e9df0d4f2a9dbdf9db4379dfc76b32d5a
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-02 22:08:09 -0500

    imported our stuff

---
+++ checkbox/checkbox.js
@@ -0,0 +1,16 @@
+import {h, Component}					from 'preact/preact';
+
+import UIButton							from '../button/button-div';	// NOTE: we don't need -link
+import UIIcon							from '../icon/icon';
+
+export default class UICheckbox extends Component {
+	render( props ) {
+		const iconName = (props.radio ? 'radio' : 'checkbox') + (props.value ? '-checked' : '-unchecked');
+		return (
+			<UIButton class={cN('ui-checkbox', props.class)} onclick={props.onclick} >
+				<UIIcon name={iconName} />
+				<span class="-text">{props.children}</span>
+			</UIButton>
+		);
+	}
+}
